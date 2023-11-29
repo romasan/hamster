3490b1aa4074bde837c9b1d675ecf6fa9c1f44e5
Roman Bauer <r.bauer@vk.team>
2023-11-30 02:49:27 +0300

    debug

--- pages/test/firebase_subscribe.js
+++ pages/test/firebase_subscribe.js
@@ -1,6 +1,3 @@
-mportScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
-
 firebase.initializeApp({
 	messagingSenderId: '1061805458854'
 });
---
+++ static/firebase-messaging-sw.js
@@ -0,0 +1,6 @@
+mportScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
+
+firebase.initializeApp({
+	messagingSenderId: '1061805458854'
+});
