622c4f5550250959d75575d6c2fcad519ec3a789
Roman Bauer <r.bauer@vk.team>
2023-11-30 03:05:45 +0300

    debug

--- static/firebase-messaging-sw.js
+++ static/firebase-messaging-sw.js
@@ -1,4 +1,4 @@
-mportScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
 
 firebase.initializeApp({
